1c6d747a309df05e92ea018d5607da7f9627ab7e
Sakina Kagalwala <sakku.kagalwala@gmail.com>
2021-05-31 22:43:39 +0530

    Added Form Validation

---
+++ script.js
@@ -0,0 +1,76 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//check for the required fields
+function checkRequired(inputArr){
+    inputArr.forEach(function (input){
+        if(input.value ===''){
+            showError(input,`${getName(input)} is required`)
+        }else{
+            showSuccess(input)
+        }
+    })
+}
+
+// get the names of the fields
+function getName(input){
+    return input.id.charAt(0).toUpperCase()+input.id.slice(1)
+}
+
+//check length of username and password
+function checkLength(input,min,max){
+    if(input.value.length < min){
+        showError(input,`${getName(input)} should be atleast of ${min} characters`)
+    }else if(input.value.length > max){
+        showError(input,`${getName(input)} should be less than ${max} characters`)
+    }else{
+        showSuccess(input)
+    }
+}
+
+//show error message
+function showError(input,message){
+    const formControl = input.parentElement
+    formControl.className = 'form-control error'
+    const small = formControl.querySelector('small')
+    small.innerText = message
+}
+
+//show success
+function showSuccess(input){
+    const formControl = input.parentElement
+    formControl.className = 'form-control success'
+}
+
+//email validation
+function emailValidator(input){
+    const regEx = /\S+@\S+\.\S+/
+    // const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(regEx.test(input)){
+        showSuccess(input)
+    }else{
+        showError(input,`${getName(input)} is not valid`)
+    }
+}
+
+//password match
+function checkPasswordMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2,`${getName(input2)} does not match`)
+    }
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    checkRequired([username,email,password,password2])
+    checkLength (username,3,15)
+    checkLength(password,6,12)
+    emailValidator(email)
+    checkPasswordMatch(password,password2)
+});
+
+console.log("hi")
